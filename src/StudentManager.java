9c1646a75b0bc777258d18c96b732770406a006d
Bjornar Aarset <32538579+b3arrr@users.noreply.github.com>
2024-09-05 15:47:25 +0200

    changed name on classes and improved user experience

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -2,23 +2,55 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Test {
+public class StudentManager {
 
 
     public static void main(String[] args) {
+        GradeManager studentManager = new GradeManager();
 
-    GradeManager studentManager = new GradeManager();
 
-    /*studentManager.makeStudent();
-    studentManager.makeStudent();
+        int userInput = startProgram();
+        System.out.println("user typed: " + userInput);
+        boolean loop = false;
+        while (!loop) {
+            newStudent(studentManager);
 
-    studentManager.listStudents(); //list all students
-    studentManager.gradeAverage(); //prints out the average grade for students
-    studentManager.displayGradeHigh(); //prints out the student with the highest grade
-    studentManager.displayGradeLow(); //prints out the student with the lowest grade*/
+        }
+
+
+
+
+    }
+    public  static void newStudent(GradeManager studentManager) {
+        studentManager.makeStudent();
+
+    }
+    public static int startProgram () {
+        System.out.println("--Welcome to the student manager program--\nWhat do you wish to do?");
+        Scanner userInput = new Scanner(System.in);
+        String userString;
+        int numberInput = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.println("Please select one of the following options:");
+            System.out.println("1. Add student to database\n2. See student with highest grade\n3. See student with lowest grade.4. See the average grade of the students\n5. Quit application");
+            try {
+                userString = userInput.nextLine();
+                numberInput = Integer.parseInt(userString);
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input, please use a numeric value between 1-5");
+                userInput.nextLine();
+            }
+
+
+        }
+        return numberInput;
 
     }
 }
+
+
 
 class Student {  // Student class with attributes name and grade. This is to be used as object for the grade manager class
 
@@ -45,13 +77,13 @@ class Student {  // Student class with attributes name and grade. This is to be 
 
 }
 
-class GradeManager
-{
+class GradeManager {
     public ArrayList<Student> students;
 
     public GradeManager() {
         students = new ArrayList<>();
     }
+
     public void makeStudent() {
         Scanner userInput = new Scanner(System.in);
 
@@ -78,19 +110,22 @@ class GradeManager
         System.out.println(output);
 
     }
+
     public String addStudent(Student student) {
         students.add(student);
         return "There are " + students.size() + " student(s) in the array";
 
     }
+
     public void listStudents() {
         int i = 1;
         for (Student student : students) {
 
-            System.out.println("Student: " + i + "\nName: " + student.getName() + " \nGrade: " + student.getGrade() +"\n");
+            System.out.println("Student: " + i + "\nName: " + student.getName() + " \nGrade: " + student.getGrade() + "\n");
             i++; //Increments student ID with 1 integer
         }
     }
+
     public void gradeAverage() {
 
         double gradeTotal = 0;
@@ -103,28 +138,29 @@ class GradeManager
         System.out.println("The average grade is " + gradeAverage);
     }
 
-    public void displayGradeHigh () {
+    public void displayGradeHigh() {
         double highGrade = 0;
         String highGradeName = null;
         for (Student student : students) {
             double grade = student.getGrade();
-            if(grade > highGrade) {
+            if (grade > highGrade) {
                 highGrade = grade;
                 highGradeName = student.getName();
             }
         }
-        System.out.println("The student with the highest grade is " + highGradeName +" with a grade of " + highGrade);
+        System.out.println("The student with the highest grade is " + highGradeName + " with a grade of " + highGrade);
     }
-    public void displayGradeLow () {
+
+    public void displayGradeLow() {
         double LowGrade = Double.MAX_VALUE;
         String LowGradeName = null;
         for (Student student : students) {
             double grade = student.getGrade();
-            if(grade < LowGrade) {
+            if (grade < LowGrade) {
                 LowGrade = grade;
                 LowGradeName = student.getName();
             }
         }
-        System.out.println("The student with the lowest grade is " + LowGradeName +" with a grade of " + LowGrade);
+        System.out.println("The student with the lowest grade is " + LowGradeName + " with a grade of " + LowGrade);
     }
 }
